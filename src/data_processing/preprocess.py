cd625709e0c6f0e749795d2e396996c962432542
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-04-18 19:35:38 -0400

    script to correctly format raw dataset

---
+++ src/data_processing/preprocess.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+import nltk
+from nltk.tokenize import word_tokenize
+import string
+from os import path
+import pandas as pd
+
+
+def print_padded_sequences(padded_sequences) -> None:
+    df_sequences = pd.DataFrame(padded_sequences)
+    print(df_sequences.head())
+
+
+def clean_text(text) -> list[str]:
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    words = word_tokenize(text)
+
+    return words
+
+
+def build_vocab(tokenized_lines) -> list[list[int]] and tf.keras.preprocessing.text.Tokenizer:
+    tokenizer = tf.keras.preprocessing.text.Tokenizer()
+    tokenizer.fit_on_texts(tokenized_lines)
+    sequences = tokenizer.texts_to_sequences(tokenized_lines)
+
+    return sequences, tokenizer
+
+
+def get_padded_sequences_and_tokenizer() -> tf.Tensor and tf.keras.preprocessing.text.Tokenizer:
+    file_path = path.join(path.dirname(path.realpath(__file__)), '../../data', 'raw')
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    tokenized_lines = [clean_text(line) for line in lines]
+
+    sequences, tokenizer = build_vocab(tokenized_lines)
+    padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(sequences, padding='post')
+
+    return padded_sequences, tokenizer
+
+
+def main() -> None:
+    padded_sequences = get_padded_sequences_and_tokenizer()
+
+    print_padded_sequences(padded_sequences)
+
+
+if __name__ == '__main__':
+    nltk.download('punkt')
+    main()
