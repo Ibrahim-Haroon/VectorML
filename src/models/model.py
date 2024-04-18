ebccd50f1c0a5d9d63e9c5a5b63f58c1bd68b1d2
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-04-18 19:33:52 -0400

    script to train ML model on processed dataset.

---
+++ src/models/model.py
@@ -0,0 +1,60 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, Flatten, Dense
+from src.data_processing.preprocess import get_padded_sequences_and_tokenizer
+import numpy as np
+from os import path
+
+
+def build_model(
+        vocab_size: int,
+        output_dim: int = 300,
+) -> Sequential:
+    model = Sequential([
+        Embedding(input_dim=vocab_size, output_dim=output_dim),
+        Flatten(),
+        Dense(vocab_size, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam', loss='binary_crossentropy')
+
+    model.summary()
+
+    return model
+
+
+def train_model(
+        model: Sequential,
+        input_sequences: np.ndarray,
+        target_sequences: np.ndarray,
+        save_path: str,
+        epochs: int = 10,
+        batch_size: int = 64,
+        validation_split: float = 0.2
+) -> None:
+    model.fit(
+        input_sequences,
+        target_sequences,
+        epochs=epochs,
+        batch_size=batch_size,
+        validation_split=validation_split
+    )
+
+    model.save(save_path)
+
+
+def main(
+
+) -> None:
+    save_path = path.join(path.dirname(path.realpath(__file__)), "../", "other/ml_model.keras")
+
+    padded_sequences, tokenizer = get_padded_sequences_and_tokenizer()
+    vocab_size = len(tokenizer.word_index) + 1
+    input_sequences = np.array(padded_sequences)
+    target_sequences = np.zeros((input_sequences.shape[0], 1))
+
+    model = build_model(vocab_size, padded_sequences)
+    train_model(model, input_sequences, target_sequences, save_path)
+
+
+if __name__ == '__main__':
+    main()
