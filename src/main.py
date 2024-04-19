400eea133571629dbe140cec06f8706c070f1d7e
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-04-18 20:24:39 -0400

    Reconfigure LFS and re-add large file

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-# TODO: demonstrate autocorrect using embedding model
+from src.models.predict import get_embedding
+
+
+def get_user_input() -> str:
+    return input("Enter a string: ")
+
+
+def main() -> None:
+    embedding = get_embedding("foo")
+
+
+if __name__ == "__main__":
+    main()
