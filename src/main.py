3f65b14079bf587e3a50894cb585d44f110b8081
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-04-21 22:02:57 -0400

    implemented extract_weather function which extracts the weather from the user input using regex

--- src/main.py
+++ src/main.py
@@ -1,15 +1,38 @@
+import re
 from pprint import pprint
 from src.models.predict import get_embedding
 
 
+'''
+Setup:
+- Redis
+
+Steps:
+1. Ask user for input regarding today's weather. Ex "Today is raining outside, what should I where?"
+2. Extract the weather (using regex)
+3. Preform a similarity search with the weather and arbitrary clothing fit from DB
+'''
+
+
+def extract_weather(user_input: str) -> str:
+    weather_patterns = r'\b(sunny|cloudy|rainy|raining|partly cloudy|hail|hailing|snowing|snowy|windy|wind)\b'
+
+    matches = [match for match in re.findall(weather_patterns, user_input) if match]
+
+    return matches[0]
+
+
+
 def get_user_input() -> str:
-    return input("Enter a string: ")
+    return input("Enter a question regarding clothing: ")
 
 
 def main() -> None:
     user_input = get_user_input()
-    embedding = get_embedding(user_input)
-    pprint(embedding)
+
+    weather = extract_weather(user_input)
+
+    pprint(weather)
 
 
 if __name__ == "__main__":
