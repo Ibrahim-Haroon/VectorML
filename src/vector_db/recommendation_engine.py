98a885791b6073e059a70af78c36ddef2b48005b
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-04-22 12:08:07 -0400

    reformatted fucntion definitions

--- src/vector_db/recommendation_engine.py
+++ src/vector_db/recommendation_engine.py
@@ -5,7 +5,9 @@ from redis.commands.search.query import Query
 from src.models.predict import get_embedding
 
 
-def similarity_search(conn: Redis, query_vector: np.ndarray):
+def similarity_search(
+        conn: Redis, query_vector: np.ndarray
+) -> []:
     query = (
         Query(f"(@weather_vector:{{ $vec }})=>[KNN 1 @weather_vector $vec as score]")
         .sort_by("score")
@@ -19,7 +21,9 @@ def similarity_search(conn: Redis, query_vector: np.ndarray):
     return conn.ft("idx").search(query, query_params).docs
 
 
-def get_clothing_suggestion(weather: str, conn: Redis):
+def get_clothing_suggestion(
+        weather: str, conn: Redis
+) -> []:
     weather_embedding = get_embedding(weather)
 
     return similarity_search(conn, weather_embedding)
