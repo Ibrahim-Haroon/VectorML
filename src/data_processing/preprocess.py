c8f0b331b5a2f2da14e878676fa7c66cbc0293e6
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-04-22 13:58:39 -0400

    added docustrings

--- src/data_processing/preprocess.py
+++ src/data_processing/preprocess.py
@@ -1,3 +1,6 @@
+"""
+This module is responsible for preprocessing the raw data and saving it to a csv file.
+"""
 import tensorflow as tf
 import nltk
 from nltk.tokenize import word_tokenize
@@ -7,15 +10,27 @@ import pandas as pd
 
 
 def print_padded_sequences(
-        padded_sequences
+        padded_sequences: tf.Tensor
 ) -> None:
+    """
+    @purpose: Print the padded sequences
+    :param padded_sequences: tf.Tensor
+    :rtype: None
+    :return: None
+    """
     df_sequences = pd.DataFrame(padded_sequences)
     print(df_sequences.head())
 
 
 def clean_text(
-        text
+        text: str
 ) -> list[str]:
+    """
+    @purpose: Clean the text by removing punctuation and tokenizing
+    :param text: str
+    :rtype: list[str]
+    :return: list of words
+    """
     text = text.lower()
     text = text.translate(str.maketrans('', '', string.punctuation))
     words = word_tokenize(text)
@@ -24,8 +39,14 @@ def clean_text(
 
 
 def build_vocab(
-        tokenized_lines
+        tokenized_lines: list[str]
 ) -> list[list[int]] and tf.keras.preprocessing.text.Tokenizer:
+    """
+    @purpose: Tokenize the text and build a vocabulary
+    :param tokenized_lines: list of tokenized lines
+    :rtype: list of sequences, tokenizer
+    :return: sequences, tokenizer
+    """
     tokenizer = tf.keras.preprocessing.text.Tokenizer()
     tokenizer.fit_on_texts(tokenized_lines)
     sequences = tokenizer.texts_to_sequences(tokenized_lines)
@@ -36,6 +57,11 @@ def build_vocab(
 def get_padded_sequences_and_tokenizer(
 
 ) -> tf.Tensor and tf.keras.preprocessing.text.Tokenizer:
+    """
+    @purpose: Load data from raw file, clean it, tokenize it, and pad it
+    :rtype: tf.Tensor, tf.keras.preprocessing.text.Tokenizer
+    :return: padded_sequences, tokenizer
+    """
     file_path = path.join(path.dirname(path.realpath(__file__)), '../../data', 'raw')
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
@@ -49,8 +75,14 @@ def get_padded_sequences_and_tokenizer(
 
 
 def save_padded_sequences(
-        padded_sequences
+        padded_sequences: tf.Tensor
 ) -> None:
+    """
+    @purpose: Save processed data to a csv file
+    :param padded_sequences: tf.Tensor
+    :rtype: None
+    :return: None
+    """
     processed_data_path = path.join(path.dirname(path.realpath(__file__)), '../../data', 'processed.csv')
     df_sequences = pd.DataFrame(padded_sequences)
     df_sequences.to_csv(processed_data_path, index=False)
